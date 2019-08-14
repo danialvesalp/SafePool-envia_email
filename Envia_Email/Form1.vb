8fa3db2996fcf9b2f25aa26c4784770f9ee6c544
Danielle Dias <danialvesalp@hotmail.com>
2019-08-14 00:13:59 -0300

    first commit

---
+++ Envia_Email/Form1.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Net.Mail
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Try
+            Dim servidorsmtp As New SmtpClient()
+            Dim email As New MailMessage()
+            servidorsmtp.Credentials = New _
+                Net.NetworkCredential("piscinasegura306@gmail.com", "ps123456")
+
+            servidorsmtp.Port = 25
+            servidorsmtp.Host = "smtp.gmail.com"
+            servidorsmtp.EnableSsl = True
+
+            email = New MailMessage
+            email.From = New MailAddress("piscinasegura306@gmail.com")
+            email.To.Add("daniellelima@gea.inatel.br")
+            email.Subject = "Piscina Segura"
+            email.Body = "Sua piscina foi invadida!"
+            servidorsmtp.Send(email)
+            MsgBox("Email Enviado!")
+        Catch ex As Exception
+            MsgBox("ERRO!")
+        End Try
+
+    End Sub
+End Class
---
+++ Envia_Email/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Envia_Email")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Envia_Email")>
+<Assembly: AssemblyCopyright("Copyright ©  2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("786825ac-0580-4897-a3d6-4c51c33d19b8")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
